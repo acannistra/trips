2cabb2f32098fbfc94dedde173b6febb2111ec9e
Anthony Cannistra <tony.cannistra@gmail.com>
2013-08-30 22:50:51 -0400

    turns out I'm an idiot

--- public/js/main.js
+++ public/js/main.js
@@ -264,7 +264,7 @@ function addMarker(tripdata) {
           position: results[0].geometry.location,
           id: tripdata.id,
           map: map,
-          title: tripdata.title + " – click for directions"
+          title: tripdata.title + " – click for directions",
           icon: 'img/ico/'+tripdata.category+'.png',
           dom: tripdata.id,
           center: results[0].geometry.location
